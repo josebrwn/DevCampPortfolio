aa37b290c8459cad0c042acd2ed68d4cc58bee82
joseph brown <josebrwn@gmail.com>
2018-06-18 11:46:39 -0700

    custom layouts blog & portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
-  get 'about-me', to: 'pages#about'
+  get 'about-me', to: 'pages#about' # this becomes about_me_path
   get 'contact', to: 'pages#contact'
 
   resources :blogs do
