deea7341297778ee8a782acfda09044e2c266883
joseph brown <josebrwn@gmail.com>
2018-06-27 15:18:43 -0700

    actioncable broadcast

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 
 Rails.application.routes.draw do
-  resources :comments
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios, except: [:show] do
     put :sort, on: :collection
@@ -11,12 +10,14 @@ Rails.application.routes.draw do
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   get 'tech-news', to: 'pages#tech_news'
-  
+
   resources :blogs do
     member do
       get :toggle_status
     end
   end
-  
+
+  mount ActionCable.server => '/cable'
+
   root to: 'pages#home'
 end
